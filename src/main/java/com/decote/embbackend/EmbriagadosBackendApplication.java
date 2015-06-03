8f18202b2acae7b0c30d81eacd47d95fe18a119b
Pablitoh <pabloconde88@gmail.com>
2015-06-03 14:10:50 -0300

    First real commit

---
+++ src/main/java/com/decote/embbackend/EmbriagadosBackendApplication.java
@@ -0,0 +1,16 @@
+package com.decote.embbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.decote.controllers")
+@EnableAutoConfiguration
+public class EmbriagadosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbriagadosBackendApplication.class, args);
+    }
+}
