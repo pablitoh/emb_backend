c692732fae60cf6bcb19d20b231c4d7b68155bec
Pablitoh <pabloconde88@gmail.com>
2015-06-04 10:23:45 -0300

    Added changes

--- src/test/java/com/decote/embbackend/EmbriagadosBackendApplicationTests.java
+++ src/test/java/com/decote/embbackend/EmbriagadosBackendApplicationTests.java
@@ -1,10 +1,15 @@
 package com.decote.embbackend;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EmbriagadosBackendApplication.class)
@@ -12,7 +17,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class EmbriagadosBackendApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws SQLException, URISyntaxException {
+//		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//		  String username = dbUri.getUserInfo().split(":")[0];
+//		  String password = dbUri.getUserInfo().split(":")[1];
+//		  String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+//
+//		  DriverManager.getConnection(dbUrl, username, password);
+	    
 	}
-
+	
 }
